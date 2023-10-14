67fcb8fd68270027b73fd356243f84f6ad4b0ca9
ftrajano <ftrajano@gmail.com>
2023-10-14 14:10:53 +0100

    first commit

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+# calculator.py
+# A program that simulates a calculator.
+
+# Importing the necessary modules.
+import os
+
+# import os
+# os.system('cls' if os.name == 'nt' else 'clear')
+
+# defining function that prints the menu
+def menu():
+	print('#######################')
+	print('')
+	print("Choose an operation:")
+	print("0 - para adição.")
+	print("1 - para subtração.")
+	print("2 - para multiplicação.")
+	print("3 - para adição.")
+	print('')
+	print('#######################')
+
+menu()
+
